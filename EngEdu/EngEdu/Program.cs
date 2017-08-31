47f0eef674a6e14484109b706c758bd6864815a2
minwichita <59030206@kmitl.ac.th>
2017-08-31 13:56:37 +0700

    2nd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -11,7 +11,8 @@ namespace EngEdu
         static void Main(string[] args)
         {
             Console.WriteLine("Year 2559 semester 1,I am an Engedu freshman.");
-            Console.WriteLine("Tear 2559 semester 2,I learn to progarm in C sharp");
+            Console.WriteLine("Year 2559 semester 2,I learn to progarm in C sharp.");
+            Console.WriteLine("Year 2560 semester 1,I start my programmer life.");
            
         }
     }
